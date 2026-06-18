a498705cf115455c7f503f12511634651dc01c6f
fran <125706199+YooPuiPui@users.noreply.github.com>
2026-06-18 18:50:48 -0400

    se mejora la vista del front de /pacientes, se une con el backend y muestra datos que existen en la db

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,37 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import DashboardLayout from "./components/layout/DashboardLayout";
-import PatientList from "./components/pacientes/listaPacientes"; 
+import PatientList from "./components/pacientes/listaPacientes";
 import Login from "./components/login/Login";
+import CrearControl from "./components/controles/crearControl";
 
+<Route path="/nuevo-control" element={<CrearControl />} />
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route 
-          path="/dashboard" 
+        <Route
+          path="/dashboard"
           element={
             <DashboardLayout>
               <div className="p-6">pronto...</div>
             </DashboardLayout>
-          } 
+          }
         />
-    
-        <Route 
-          path="/pacientes" 
+
+        <Route
+          path="/pacientes"
           element={
             <DashboardLayout>
               <PatientList />
             </DashboardLayout>
-          } 
+          }
+        />
+
+        <Route
+          path="/nuevo-control" 
+          element={<CrearControl />}
         />
       </Routes>
     </BrowserRouter>
